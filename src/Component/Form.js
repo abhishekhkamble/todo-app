f8391622d2b21ae7d276b054ba99a8f34e55a0c8
abhishek36 <akamble9913@gmail.com>
2021-04-13 17:31:30 +0530

    asdfsdf

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {v4 as uuidV4} from 'uuid'
 function Form({input, setInput, todos,setTodo , editTodo , setEditTodo}) {
+    const [isHidden , setIsHidden] = useState("")
     const OnInputChange = (event)=>{
         setInput(event.target.value)
+        setIsHidden(!isHidden)
+    }
+    const OnButtonClick = (event)=>{
+        if(!event.target.value)
+        {
+
+        }
     }
     // const updateTodo = (title , id , completed)=>{
     //     const newTodo = todos.map((todo)=>
@@ -22,9 +30,9 @@ function Form({input, setInput, todos,setTodo , editTodo , setEditTodo}) {
         // }
     }
     return (
-        <form onSubmit={onFormSubmit}>
-            <input type="text" className="task-input txt-input" placeholder="enter todo" value={input} onChange={OnInputChange}/>
-            <button type="submit" className="button-add"> add</button>
+        <form onSubmit={onFormSubmit} className="li-display">
+            <input type="text" name ="addItem" className="task-input txt-input" placeholder="enter todo" value={input} onChange={OnInputChange}/>
+            <button type="submit" className="button-add "> add</button>
         </form>
     )
 }
