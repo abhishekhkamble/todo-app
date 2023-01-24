d79080c08904beeda06febe8b8eb100d11b90892
Adarsh M <adarshm954@gmail.com>
2023-01-24 03:02:38 +0000

    ui responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React , {useState} from 'react'
-import './App.css';
-import Header from './Component/Header'
-import Form from './Component/Form'
-import TodoList from './Component/TodoList'
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Component/Header";
+import Form from "./Component/Form";
+import TodoList from "./Component/TodoList";
+
 function App() {
-  const [input, setInput] = useState("")
-  const [todo, setTodo] = useState([])
-  const [editTodo, seteditTodo] = useState(null)
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [editTodo, seteditTodo] = useState(null);
   return (
     <div className="container">
       <div className="app">
-      <Header/>
-      <Form 
-      input = {input}
-      setInput = {setInput}
-      todos = {todo}
-      setTodo = {setTodo}
-      editTodo={editTodo}
-      seteditTodo={seteditTodo}
-      />
-      <TodoList todos ={todo} setTodos={setTodo} seteditTodo={seteditTodo}/>
-    </div>
+        <Header />
+        <Form
+          input={input}
+          setInput={setInput}
+          todos={todo}
+          setTodo={setTodo}
+          editTodo={editTodo}
+          seteditTodo={seteditTodo}
+        />
+        <TodoList todos={todo} setTodos={setTodo} seteditTodo={seteditTodo} />
+      </div>
     </div>
   );
 }
--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,42 +1,60 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-function TodoList({todos ,setTodos , setEditTodos}) {
+function TodoList({ todos, setTodos, setEditTodos }) {
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    const handleDelete = ({id})=>{
-        setTodos(todos.filter((todo)=>todo.id !== id))
-    }
-
-    const handleComplete = (todo)=>{
-        setTodos(todos.map((item)=>{
-            if(item.id === todo.id){
-                return{...item , completed : !item.completed}
-                }
-                return item
-        }))}
-    // const handleEdit = ({id})=>{
-    //     const findTodo = todos.find((todo)=>todo.id === id);
-    //     setEditTodos(findTodo)
-    // }
-    return (
-        <div>
-            {todos.map((todo)=>(
-                <li key={todo.id} className="list-item li-display">
-                    <input type="text" className={`list ${todo.completed ? "complete":""}` } value={todo.title} onChange={(event)=>event.preventDefault()}/>
-                    <div>
-                        <button className="button-complete" onClick={()=>handleComplete(todo)}>
-                            <i className="fa fa-check-circle" ></i>
-                        </button>
-                        {/* <button   onClick={()=>handleEdit(todo)}>
-                            <i className="fa fa-edit"></i>
-                        </button> */}
-                        <button onClick={()=>handleDelete(todo)} className="button-delete">
-                            <i className="fa fa-trash"></i>
-                        </button>
-                    </div>
-                </li>
-            ))}
-        </div>
-    )
+  const handleComplete = (todo) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+  // const handleEdit = ({id})=>{
+  //     const findTodo = todos.find((todo)=>todo.id === id);
+  //     setEditTodos(findTodo)
+  // }
+  return (
+    <Container>
+      <Row>
+        {todos.map((todo) => (
+          <Col xs={12} sm={6} md={4} lg={3} xl={2} key={todo.id}>
+            <li className="list-item li-display">
+              <input
+                type="text"
+                className={`list ${todo.completed ? "complete" : ""}`}
+                value={todo.title}
+                onChange={(event) => event.preventDefault()}
+              />
+              <div>
+                <button
+                  className="button-complete"
+                  onClick={() => handleComplete(todo)}
+                >
+                  <i className="fa fa-check-circle"></i>
+                </button>
+                {/* <button   onClick={()=>handleEdit(todo)}>
+                      <i className="fa fa-edit"></i>
+                    </button> */}
+                <button
+                  onClick={() => handleDelete(todo)}
+                  className="button-delete"
+                >
+                  <i className="fa fa-trash"></i>
+                </button>
+              </div>
+            </li>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-export default TodoList
+export default TodoList;
